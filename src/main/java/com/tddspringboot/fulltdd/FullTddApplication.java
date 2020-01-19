1d667a7f68fcf14903f357fab5bdb94cb48581d7
davidjimenez0 <djimenez0315@gmail.com>
2020-01-19 18:07:57 -0500

    initial commit for tdd-spring-boot

---
+++ src/main/java/com/tddspringboot/fulltdd/FullTddApplication.java
@@ -0,0 +1,13 @@
+package com.tddspringboot.fulltdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tddspringboot/fulltdd/FullTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tddspringboot.fulltdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
